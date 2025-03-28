094bf6a3b9e7a9c21efb0ffde2814a1483350091
unknown <patrickgsiqueira@hotmail.com>
2025-03-28 12:26:54 -0300

    Agile Projets

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -21,13 +21,13 @@ const AppRoutes = () => {
         <Route path="/professionals" element={<Professionals />} />
         <Route path="/projects" element={<Projects />} />
         
-        <Route path="/professionals/maria" element={<Maria />} />
-        <Route path="/professionals/patrick" element={<Patrick />} />
-        <Route path="/professionals/giovani" element={<Giovani />} />
-        <Route path="/professionals/ranathan" element={<Ranathan />} />
-        <Route path="/professionals/renan" element={<Renan />} />
-        <Route path="/professionals/mateus" element={<Mateus />} />
-        <Route path="/professionals/joao" element={<Joao />} />
+        <Route path="/professional/maria" element={<Maria />} />
+        <Route path="/professional/patrick" element={<Patrick />} />
+        <Route path="/professional/giovani" element={<Giovani />} />
+        <Route path="/professional/ranathan" element={<Ranathan />} />
+        <Route path="/professional/renan" element={<Renan />} />
+        <Route path="/professional/mateus" element={<Mateus />} />
+        <Route path="/professional/joao" element={<Joao />} />
         <Route path="/testepage" element={<testepage />} />
         <Route path="/sobre" element={<Agile />} />
 
@@ -37,3 +37,20 @@ const AppRoutes = () => {
 };
 
 export default AppRoutes;
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "../components/Layout"; // Importando o Layout
+import App from "../App";
+import Professionals from "../pages/Professionals/Professionals";
+import Projects from "../pages/Projects/Projects";
+
+import Maria from "../pages/Professionals/Maria/Maria";
+import Patrick from "../pages/Professionals/Patrick/Patrick";
+import Giovani from "../pages/Professionals/Giovani/Giovani";
+import Ranathan from "../pages/Professionals/Ranathan/Ranathan";
+import Renan from "../pages/Professionals/Renan/Renan";
+import Mateus from "../pages/Professionals/Mateus/Mateus";
+import Joao from "../pages/Professionals/Joao/Joao";
+import Agile from "../pages/Sobre/Agile";
+import TestePage from "../pages/testepage"; // Corrigi o nome para TestePage
+
