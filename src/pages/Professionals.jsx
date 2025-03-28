dfafdf931301a3a2a699829cbc5698702a2d5b41
unknown <patrickgsiqueira@hotmail.com>
2025-03-28 12:59:07 -0300

    Agile Projets

--- src/pages/Professionals.jsx
+++ src/pages/Professionals.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Pro from "../professionals-components/AnotherProfessionals/Pro";
 import Header from "../components/Header/Header";
-import '../../App.css';
+import '../App.css';
 
 const Professionals = () => {
 
