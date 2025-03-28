b3daeb48e42ea6f0ba45839f7bc5f940826ca093
unknown <patrickgsiqueira@hotmail.com>
2025-03-28 12:24:27 -0300

    Agile Projets

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,39 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "../components/Layout"; // Importando o Layout
-import App from "../App";
-import Professionals from "../pages/Professionals/Professionals";
-import Projects from "../pages/Projects/Projects";
+import App from "../App"; // Ajuste o caminho conforme necessário
+import Professionals from "../pages/Professionals/Professionals"; // Ajuste o caminho conforme necessário
+import Projects from '../pages/Projects/Projects'; // Importe a página Projects
 
-import Maria from "../pages/Professionals/Maria/Maria";
-import Patrick from "../pages/Professionals/Patrick/Patrick";
-import Giovani from "../pages/Professionals/Giovani/Giovani";
-import Ranathan from "../pages/Professionals/Ranathan/Ranathan";
-import Renan from "../pages/Professionals/Renan/Renan";
-import Mateus from "../pages/Professionals/Mateus/Mateus";
-import Joao from "../pages/Professionals/Joao/Joao";
-import Agile from "../pages/Sobre/Agile";
-import TestePage from "../pages/testepage"; // Corrigi o nome para TestePage
+import Maria from "../pages/Professionals/Maria/Maria"; // Ajuste o caminho conforme necessário
+import Patrick from '../pages/Professionals/Patrick/Patrick'; // Importe a página Patrick
+import Giovani from '../pages/Professionals/Giovani/Giovani'; // Importe a página Giovani
+import Ranathan from '../pages/Professionals/Ranathan/Ranathan'; // Importe a página Ranathan
+import Renan from '../pages/Professionals/Renan/Renan'; // Importe a página Renan
+import Mateus from '../pages/Professionals/Mateus/Mateus'; // Importe a página Mateus
+import Joao from '../pages/Professionals/Joao/Joao'; // Importe a página Mateus
+import Agile from '../pages/Sobre/Agile'; // Importe a página Mateus
+import testepage from '../pages/testepage'; // Importe a página Mateus
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        {/* O Layout envolve todas as páginas e renderiza o Footer */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="/professionals" element={<Professionals />} />
-          <Route path="/projects" element={<Projects />} />
-          
-          <Route path="/professionals/maria" element={<Maria />} />
-          <Route path="/professionals/patrick" element={<Patrick />} />
-          <Route path="/professionals/giovani" element={<Giovani />} />
-          <Route path="/professionals/ranathan" element={<Ranathan />} />
-          <Route path="/professionals/renan" element={<Renan />} />
-          <Route path="/professionals/mateus" element={<Mateus />} />
-          <Route path="/professionals/joao" element={<Joao />} />
-          <Route path="/testepage" element={<TestePage />} />
-          <Route path="/sobre" element={<Agile />} />
-        </Route>
+        <Route path="/" element={<App />} />
+        <Route path="/professionals" element={<Professionals />} />
+        <Route path="/projects" element={<Projects />} />
+        
+        <Route path="/professionals/maria" element={<Maria />} />
+        <Route path="/professionals/patrick" element={<Patrick />} />
+        <Route path="/professionals/giovani" element={<Giovani />} />
+        <Route path="/professionals/ranathan" element={<Ranathan />} />
+        <Route path="/professionals/renan" element={<Renan />} />
+        <Route path="/professionals/mateus" element={<Mateus />} />
+        <Route path="/professionals/joao" element={<Joao />} />
+        <Route path="/testepage" element={<testepage />} />
+        <Route path="/sobre" element={<Agile />} />
+
       </Routes>
     </Router>
   );
